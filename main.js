83514b9636f5e5ca3d6b85c61d847c8039cbbed1
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2015-08-06 21:21:47 -0400

    Remove unnecessary sound key

--- main.js
+++ main.js
@@ -34,9 +34,8 @@ app.post('/quarantine', function (req, res) {
 
     var notification = new apn.Notification({'quarantine': 1});
 
-    notification.priority = 5;;
+    notification.priority = 5;
     notification.contentAvailable = true;
-    notification.sound = "";
 
     apnConnection.pushNotification(notification, device);
 
